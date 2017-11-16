b75787b8c49e27e91fe0855d9fa14c198c8c92e7
Danny <DJ@MacBook-Pro.local>
2017-11-16 11:25:59 -0700

    finished basic layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
-const layout = (props) => (
-    <Aux>
-    <Toolbar />
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+    sideDrawerOpenHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+    })
+}
+   render() { 
+   return (
+   <Aux>
+    <Toolbar openMenu={this.sideDrawerOpenHandler}/>
+    <SideDrawer 
+    open={this.state.showSideDrawer}
+    closed={this.sideDrawerCloseHandler}/>
     <main className={classes.Content}>
-        {props.children}
+        {this.props.children}
         </main>
         </Aux>
+   )}
+}
 
-);
-
-export default layout;
+export default Layout;
