6030560c1ed24c65220ac4b2111bd9a35c109ab3
Danny <DJ@MacBook-Pro.local>
2018-01-29 02:01:07 -0700

    validation added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -118,34 +118,54 @@ class ContactData extends Component {
         //     event.preventDefault()
     }
 
-    inputChangedHandler = (event, inputId) => {
+    inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
-        } 
-        const updatedFormElement = {
-            ...updatedOrderForm[inputId]
-        }
-        // updatedFormElement.touched = true;
+        };
+        const updatedFormElement = { 
+            ...updatedOrderForm[inputIdentifier]
+        };
         updatedFormElement.value = event.target.value;
-        // updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
-        updatedOrderForm[inputId] = updatedFormElement;
-        this.setState({orderForm: updatedOrderForm})
-
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
-    // checkValidity(value, rules){
-    //     let isValid = false;
-    //     if (rules.required) {
-    //         isValid = value.trim() !== '' && isValid
-    //     }
-    //     if (rules.minLength) {
-    //         isValid = value.length >= rules.minLength && isValid
-    //     }
-    //     if (rules.maxLength) {
-    //         isValid = value.length <= rules.maxLength && isValid
-    //     }
-    //     return isValid;
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
 
-    // }
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
 
     render() {
         const formElementsArray = [];
